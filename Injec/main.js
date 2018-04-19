ab83b0e04c513cf0fcb45d1c4b0c6892c83bd373
moi <moi@ici.net>
2018-04-19 12:59:03 -0400

    javascript

---
+++ Injec/main.js
@@ -0,0 +1,12 @@
+/*appel de la fonction par le symbole $ */
+jQuery(function($){
+
+/* selection de tous les input de type range */
+$('.range').each(function(){
+	var classe = $(this).attr('class');
+	 
+	
+	});
+
+
+=);
