7b14b0c227c1c7bbd685dbc72a5fd4987b6c1a17
moi <moi@ici.net>
2018-04-23 15:10:18 -0400

    genration des cycles

---
+++ Injec/cycle.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/dot-luv/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+
+  <div class="input">
+	<label for="">Durée <span></span></label>
+	<input type="text" name="durée" class="range min-0 max-5" value="2"/>
+
+</div>
+<div class="input">
+	<label for="">periode <span></span></label>
+	<input type="text" name="durée" class="range min-0 max-5" value="2"/>
+
+</div>
+<div class="input">
+<button id="button" class="ui-button ui-corner-all ui-widget">Generer</button>
+
+</div>
+</body>
+</html>
