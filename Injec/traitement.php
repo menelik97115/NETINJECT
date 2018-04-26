8f046b9263f80d047b964a3e6e77a63a5a515af4
moi <moi@ici.net>
2018-04-26 10:41:57 -0400

    gestion du cycle, affichage des valeurs

--- Injec/traitement.php
+++ Injec/traitement.php
@@ -3,14 +3,24 @@
 <title>traitement du cycle </title>
 </head>
 <body>
-<?
+<?php
 // on teste la déclaration de nos variables
-echo " hello";
-var_dump($_REQUEST);
-if (isset($_POST['duree1']) {
+
+if (!empty($_POST['duree1']) AND !empty($_POST['periode1'])){
+	if($_POST['duree1'] OR $_POST['periode1'] != 0){
 	// on affiche nos résultats
-	echo 'Votre duree '.$_POST['duree1'];
+	$duree= $_POST['duree1'];
+	$periode= $_POST['periode1'];
+	echo "Votre duree est ".$duree." Et votre periode est de ".$periode;
+	}
+}	
+else {
+header('Location: cycle.php');
 }
-else echo 'aucune donnée transmise' ;
 ?>
+
+
+
+
+
 </body>
